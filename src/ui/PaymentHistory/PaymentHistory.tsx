8e127c6dab465908ac5e9ad0b68c5563abab64fc
Alex Hrabovyi <alexgraboviypro@gmail.com>
2024-08-30 18:29:33 +0300

    Remake graph visualization algorithm to use SVG

--- src/ui/PaymentHistory/PaymentHistory.tsx
+++ src/ui/PaymentHistory/PaymentHistory.tsx
@@ -150,26 +150,29 @@ const MONTHS: Record<number, string> = {
 };
 
 export default function PaymentHistory() {
-  const canvasRef = useRef<null | HTMLCanvasElement>(null);
+  const svgWrapperRef = useRef<null | HTMLDivElement>(null);
+  const svgRef = useRef<null | SVGSVGElement>(null);
 
   const [activeDateRangeBtn, setActiveDateRangeBtn] = useState('1M');
+  const [dStrokeAttribute, setDStrokeAttribute] = useState('');
+  const [dFillAttribute, setDFillAttribute] = useState('');
 
   const allAmounts = paymentStats.map((p) => Number(p.amount));
   const minAmount = Math.min(...allAmounts);
   const maxAmount = Math.max(...allAmounts);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
+    const svgWrapper = svgWrapperRef.current;
+    const svgEl = svgRef.current;
 
-    if (!canvas) return;
+    if (!svgEl) return;
+    if (!svgWrapper) return;
 
-    const canvasWidth = canvas.offsetWidth;
-    const canvasHeight = canvas.offsetHeight;
+    const svgWidth = svgWrapper.offsetWidth;
+    const svgHeight = svgWrapper.offsetHeight;
 
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-
-    let enhancedPaymentStatsObjs;
+    svgEl.setAttribute('width', String(svgWidth));
+    svgEl.setAttribute('height', String(svgHeight));
 
     function divideDate(dateStr: string) {
       const splittedDate = dateStr.split('-');
@@ -186,7 +189,7 @@ export default function PaymentHistory() {
     }
 
     const daysAmount = allAmounts.length;
-    const XStep = Number((canvasWidth / (daysAmount - 1)).toFixed(5));
+    const XStep = Number((svgWidth / (daysAmount - 1)).toFixed(5));
 
     function calcXCoord(day: number): number {
       return Number((day * XStep - XStep).toFixed(0));
@@ -194,14 +197,14 @@ export default function PaymentHistory() {
 
     const bottomIndentPercent = 0.2;
 
-    const maxYCoord = Number((canvasHeight * (1 - bottomIndentPercent)).toFixed(0));
+    const maxYCoord = Number((svgHeight * (1 - bottomIndentPercent)).toFixed(0));
     const YStep = Number((maxYCoord / (maxAmount - minAmount)).toFixed(5));
 
     function calcYCoord(amount: number): number {
       return Math.abs(Number(((amount - minAmount) * YStep).toFixed(0)) - 140);
     }
 
-    enhancedPaymentStatsObjs = paymentStats.map((p) => {
+    const enhancedPaymentStatsObjs = paymentStats.map((p) => {
       const dividedDate = divideDate(p.date);
 
       return {
@@ -212,43 +215,27 @@ export default function PaymentHistory() {
       };
     });
 
-    const ctx = canvas.getContext('2d');
-
-    if (!ctx) return;
-
-    ctx.beginPath();
-    ctx.strokeStyle = '#0AAF60';
-    ctx.lineWidth = 2;
-    ctx.lineJoin = 'round';
-
-    const lineargradient = ctx.createLinearGradient(0, 0, 0, canvasHeight);
-    lineargradient.addColorStop(0, 'rgba(48, 197, 89, 0.7)');
-    lineargradient.addColorStop(1, 'rgba(48, 197, 89, 0)');
-    ctx.fillStyle = lineargradient;
-
     const firstStatX = enhancedPaymentStatsObjs[0].x;
     const firstStatY = enhancedPaymentStatsObjs[0].y;
-    ctx.moveTo(firstStatX, firstStatY);
+    let dStrokeStr = `M ${firstStatX} ${firstStatY}`;
 
     for (let i = 1; i < enhancedPaymentStatsObjs.length; i += 1) {
       const statObj = enhancedPaymentStatsObjs[i];
 
-      ctx.lineTo(statObj.x, statObj.y);
+      dStrokeStr += ` L ${statObj.x} ${statObj.y}`;
     }
 
-    ctx.stroke();
+    let dFillStr = dStrokeStr;
+    dFillStr += `L  ${svgWidth} ${svgHeight} L 0 ${svgHeight} Z`;
 
-    ctx.lineTo(canvasWidth, canvasHeight);
-    ctx.lineTo(0, canvasHeight);
-
-    ctx.globalAlpha = 0.2;
-    ctx.fill();
+    setDStrokeAttribute(dStrokeStr);
+    setDFillAttribute(dFillStr);
 
     console.log(enhancedPaymentStatsObjs);
 
     console.log(`
-      canvasWidth: ${canvasWidth};
-      canvasHeight: ${canvasHeight};
+      canvasWidth: ${svgWidth};
+      canvasHeight: ${svgHeight};
       maxYCoord: ${maxYCoord};
       minAmount: ${minAmount};
       maxAmount: ${maxAmount};
@@ -311,10 +298,37 @@ export default function PaymentHistory() {
         </div>
       </div>
       <div className="w-full h-full pt-[80px] flex flex-col justify-start items-start gap-[16px]">
-        <canvas
-          ref={canvasRef}
-          className="w-full h-full max-h-[175px]"
-        />
+        <div
+          ref={svgWrapperRef}
+          className="w-full h-full"
+        >
+          <svg
+            ref={svgRef}
+            className="w-full h-full"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <defs>
+              <linearGradient id="greenGradient" x1="0" x2="0" y1="0" y2="1">
+                <stop offset="0" stopColor="#30C559" stopOpacity="0.7" />
+                <stop offset="1" stopColor="#30C559" stopOpacity="0" />
+              </linearGradient>
+            </defs>
+            <path
+              d={dStrokeAttribute}
+              fill="none"
+              strokeWidth="2"
+              stroke="#0AAF60"
+              strokeLinejoin="round"
+            />
+            <path
+              d={dFillAttribute}
+              fill="url(#greenGradient)"
+              stroke="none"
+              opacity="0.2"
+            />
+          </svg>
+        </div>
         <div className="w-full px-[24px] pb-[24px] flex justify-between items-center
           font-tthoves text-[14px] text-grey-400"
         >
