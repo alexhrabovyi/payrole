42c2881d0c08f726cb2131c75de8bc4fe7ef4488
Alex Hrabovyi <alexgraboviypro@gmail.com>
2024-09-01 18:11:14 +0300

    Complete graph paint algorithm

--- src/ui/PaymentHistory/PaymentHistory.tsx
+++ src/ui/PaymentHistory/PaymentHistory.tsx
@@ -25,6 +25,20 @@ interface PaymentStatsWithCoords extends AdjustedPaymentStats {
   y: number,
 }
 
+interface StrokeProps {
+  width: string,
+  colorGreen: string,
+  colorRed: string,
+  linejoin: 'round' | 'miter' | 'bevel' | 'inherit',
+}
+
+interface PrevAdditionalCoords {
+  x: number,
+  y: number,
+}
+
+type AmountType = 'negative' | 'positive';
+
 // 31 day of the same month. Only positive numbers;
 const paymentStats: PaymentStats[] = [
   {
@@ -177,7 +191,7 @@ const paymentStats1: PaymentStats[] = [
   },
 ];
 
-// 5 days of the same month. One negative number;
+// 7 days of the same month. Three negative numbers;
 const paymentStats2: PaymentStats[] = [
   {
     date: '12-03-2023',
@@ -189,10 +203,18 @@ const paymentStats2: PaymentStats[] = [
   },
   {
     date: '12-15-2023',
-    amount: '-1432',
+    amount: '-3432',
   },
   {
     date: '12-16-2023',
+    amount: '-2451',
+  },
+  {
+    date: '12-17-2023',
+    amount: '-551',
+  },
+  {
+    date: '12-19-2023',
     amount: '2323',
   },
   {
@@ -201,7 +223,194 @@ const paymentStats2: PaymentStats[] = [
   },
 ];
 
-// it can be remade as enum
+// all days are of the same month. Positive and negative numbers;
+const paymentStats3: PaymentStats[] = [
+  {
+    date: '12-01-2023',
+    amount: '3232',
+  },
+  {
+    date: '12-04-2023',
+    amount: '311',
+  },
+  {
+    date: '12-05-2023',
+    amount: '-2351',
+  },
+  {
+    date: '12-06-2023',
+    amount: '-522',
+  },
+  {
+    date: '12-07-2023',
+    amount: '2854',
+  },
+  {
+    date: '12-10-2023',
+    amount: '561',
+  },
+  {
+    date: '12-11-2023',
+    amount: '1145',
+  },
+  {
+    date: '12-12-2023',
+    amount: '434',
+  },
+  {
+    date: '12-13-2023',
+    amount: '-1643',
+  },
+  {
+    date: '12-15-2023',
+    amount: '1132',
+  },
+  {
+    date: '12-16-2023',
+    amount: '2165',
+  },
+  {
+    date: '12-17-2023',
+    amount: '3781',
+  },
+  {
+    date: '12-18-2023',
+    amount: '1784',
+  },
+  {
+    date: '12-20-2023',
+    amount: '-411',
+  },
+  {
+    date: '12-21-2023',
+    amount: '-932',
+  },
+  {
+    date: '12-23-2023',
+    amount: '-1810',
+  },
+  {
+    date: '12-24-2023',
+    amount: '1284',
+  },
+  {
+    date: '12-25-2023',
+    amount: '2914',
+  },
+  {
+    date: '12-27-2023',
+    amount: '341',
+  },
+  {
+    date: '12-28-2023',
+    amount: '2931',
+  },
+  {
+    date: '12-30-2023',
+    amount: '-3010',
+  },
+  {
+    date: '12-31-2023',
+    amount: '-2071',
+  },
+];
+
+// all days are of the same month. Positive and negative numbers;
+const paymentStats4: PaymentStats[] = [
+  {
+    date: '12-01-2023',
+    amount: '-3232',
+  },
+  // {
+  //   date: '12-02-2023',
+  //   amount: '-1311',
+  // },
+  {
+    date: '12-04-2023',
+    amount: '-311',
+  },
+  {
+    date: '12-05-2023',
+    amount: '-2351',
+  },
+  {
+    date: '12-06-2023',
+    amount: '522',
+  },
+  {
+    date: '12-07-2023',
+    amount: '2854',
+  },
+  {
+    date: '12-10-2023',
+    amount: '561',
+  },
+  {
+    date: '12-11-2023',
+    amount: '1145',
+  },
+  {
+    date: '12-12-2023',
+    amount: '434',
+  },
+  {
+    date: '12-13-2023',
+    amount: '-1643',
+  },
+  {
+    date: '12-15-2023',
+    amount: '1132',
+  },
+  {
+    date: '12-16-2023',
+    amount: '2165',
+  },
+  {
+    date: '12-17-2023',
+    amount: '3781',
+  },
+  {
+    date: '12-18-2023',
+    amount: '1784',
+  },
+  {
+    date: '12-20-2023',
+    amount: '-411',
+  },
+  {
+    date: '12-21-2023',
+    amount: '-932',
+  },
+  {
+    date: '12-23-2023',
+    amount: '-1810',
+  },
+  {
+    date: '12-24-2023',
+    amount: '1284',
+  },
+  {
+    date: '12-25-2023',
+    amount: '2914',
+  },
+  {
+    date: '12-27-2023',
+    amount: '341',
+  },
+  {
+    date: '12-28-2023',
+    amount: '2931',
+  },
+  {
+    date: '12-30-2023',
+    amount: '-3010',
+  },
+  {
+    date: '12-31-2023',
+    amount: '-2071',
+  },
+];
+
 const MONTHS: Record<number, string> = {
   1: 'January',
   2: 'February',
@@ -222,9 +431,12 @@ export default function PaymentHistory() {
   const svgRef = useRef<null | SVGSVGElement>(null);
 
   const [activeDateRangeBtn, setActiveDateRangeBtn] = useState('1M');
+  const [adjustedPaymentStats, setAdjustedPaymentStats] = useState<AdjustedPaymentStats[]>([]);
   const [svgPaths, setSvgPaths] = useState<ReactNode[]>([]);
 
-  const adjustPaymentStats = useCallback((statsObj: PaymentStats[]) => {
+  const adjustPaymentStats = useCallback(() => {
+    const statsObj = paymentStats4;
+
     function divideDate(dateStr: string) {
       const splittedDate = dateStr.split('-');
 
@@ -257,69 +469,160 @@ export default function PaymentHistory() {
     }
 
     const adjustedPaymentStatsObj: AdjustedPaymentStats[] = [];
-
     const firstAdjustedStatsDate = divideDate(statsObj[0].date);
-
-    if (firstAdjustedStatsDate.day !== 1) {
-      fillWithPaymentStats(
-        adjustedPaymentStatsObj,
-        1,
-        firstAdjustedStatsDate.day,
-        firstAdjustedStatsDate.month,
-        firstAdjustedStatsDate.year,
-      );
-    }
 
     statsObj.forEach((obj, i) => {
       const currentObjAdjustedDate = divideDate(obj.date);
+      const currentDay = currentObjAdjustedDate.day;
+      let prevDay: number;
+
+      const prevObj = statsObj[i - 1];
+
+      if (prevObj) {
+        prevDay = divideDate(prevObj.date).day;
+      } else {
+        prevDay = 0;
+      }
+
+      const diff = currentDay - prevDay;
+
+      if (diff !== 1) {
+        fillWithPaymentStats(
+          adjustedPaymentStatsObj,
+          prevDay + 1,
+          currentDay,
+          firstAdjustedStatsDate.month,
+          firstAdjustedStatsDate.year,
+        );
+      }
 
       adjustedPaymentStatsObj.push({
         amount: Number(obj.amount),
         ...currentObjAdjustedDate,
       });
 
-      const nextObj = statsObj[i + 1];
-
-      if (!nextObj) {
-        if (currentObjAdjustedDate.day !== 31) {
-          fillWithPaymentStats(
-            adjustedPaymentStatsObj,
-            currentObjAdjustedDate.day + 1,
-            32,
-            firstAdjustedStatsDate.month,
-            firstAdjustedStatsDate.year,
-          );
-        }
-
-        return;
-      }
-
-      const nextObjAdjustedDate = divideDate(nextObj.date);
-      const currentObjDay = currentObjAdjustedDate.day;
-      const nextObjDay = nextObjAdjustedDate.day;
-
-      const daysDiff = nextObjDay - currentObjDay;
-
-      if (daysDiff !== 1) {
+      if ((i === statsObj.length - 1) && currentDay !== 31) {
         fillWithPaymentStats(
           adjustedPaymentStatsObj,
-          currentObjDay + 1,
-          nextObjDay,
+          currentDay + 1,
+          32,
           firstAdjustedStatsDate.month,
           firstAdjustedStatsDate.year,
         );
       }
     });
 
-    return adjustedPaymentStatsObj;
+    setAdjustedPaymentStats(adjustedPaymentStatsObj);
   }, []);
 
   const paintGraph = useCallback(() => {
+    const BOTTOM_INDENT_PERCENT = 0.1;
+
+    const STROKE_PROPS: StrokeProps = {
+      width: '2',
+      colorGreen: '#0AAF60',
+      colorRed: '#FA4545',
+      linejoin: 'round',
+    };
+
+    const FILL_PROPS = {
+      colorGreen: 'url(#greenGradient)',
+      colorRed: 'url(#redGradient)',
+      opacity: '0.3',
+    };
+
+    function calcXCoord(day: number, step: number) {
+      return Number((day * step - step).toFixed(0));
+    }
+
+    function calcYCoord(amount: number, step: number, minAmount: number, maxYCoord: number) {
+      return Math.abs(Number(((amount - minAmount) * step).toFixed(0)) - maxYCoord);
+    }
+
+    function checkAmountType(amount: number): AmountType {
+      return amount >= 0 ? 'positive' : 'negative';
+    }
+
+    function startPath(x: number, y: number) {
+      return `M ${x} ${y}`;
+    }
+
+    function calcNextAdditionalCoords(
+      currentX: number,
+      nextX: number,
+      currentAmount: number,
+      nextAmount: number,
+      zeroLineYCoord: number,
+    ) {
+      const ABSAmountSum = Math.abs(currentAmount) + Math.abs(nextAmount);
+      const currentAmountPercent = Math.abs(currentAmount) / ABSAmountSum;
+
+      const additionalCoordX = currentX + (nextX - currentX) * currentAmountPercent;
+      const additionalCoordY = zeroLineYCoord;
+
+      return {
+        x: additionalCoordX,
+        y: additionalCoordY,
+      };
+    }
+
+    function addGraphPaths(
+      pathsArr: ReactNode[],
+      color: 'colorGreen' | 'colorRed',
+      strokeStr: string,
+      fillStr: string,
+    ) {
+      pathsArr.push(
+        <path
+          d={strokeStr}
+          fill="none"
+          strokeWidth={STROKE_PROPS.width}
+          stroke={STROKE_PROPS[color]}
+          strokeLinejoin={STROKE_PROPS.linejoin}
+        />,
+        <path
+          d={fillStr}
+          fill={FILL_PROPS[color]}
+          stroke="none"
+          opacity={FILL_PROPS.opacity}
+        />,
+      );
+    }
+
+    function isZeroLineNeeded(
+      currentAmountType: AmountType,
+      isNextAdditionalCoordsNeeded: boolean,
+      isLast: boolean,
+      isPrevCoordsExist: boolean,
+    ) {
+      if (currentAmountType === 'negative' && (isNextAdditionalCoordsNeeded || (isLast && isPrevCoordsExist))) {
+        return true;
+      }
+
+      return false;
+    }
+
+    function drawAndAddZeroLinePath(
+      pathsArr: ReactNode[],
+      zeroLineY: number,
+      x1: number,
+      x2: number,
+    ) {
+      pathsArr.push(
+        <path
+          d={`M ${x1} ${zeroLineY} L ${x2} ${zeroLineY}`}
+          fill="none"
+          strokeWidth="2"
+          stroke="#CACACE"
+          strokeDasharray="5,5"
+        />,
+      );
+    }
+
     const svgWrapper = svgWrapperRef.current;
     const svgEl = svgRef.current;
 
-    if (!svgEl) return;
-    if (!svgWrapper) return;
+    if (!svgEl || !svgWrapper) return;
 
     const svgWidth = svgWrapper.offsetWidth;
     const svgHeight = svgWrapper.offsetHeight;
@@ -327,158 +630,165 @@ export default function PaymentHistory() {
     svgEl.setAttribute('width', String(svgWidth));
     svgEl.setAttribute('height', String(svgHeight));
 
-    const adjustedPaymentStatsObj = adjustPaymentStats(paymentStats1);
     const paths: ReactNode[] = [];
 
-    // needs to be deleted
-    let zeroLinePath: ReactNode;
-
-    const allAmounts = adjustedPaymentStatsObj.map((p) => p.amount);
+    const allAmounts = adjustedPaymentStats.map((p) => p.amount);
     const minAmount = Math.min(...allAmounts);
     const maxAmount = Math.max(...allAmounts);
 
-    const daysAmount = adjustedPaymentStatsObj.length;
-    const bottomIndentPercent = 0.2;
-    const maxYCoord = Number((svgHeight * (1 - bottomIndentPercent)).toFixed(0));
+    const daysAmount = adjustedPaymentStats.length;
+    const maxYCoord = Number((svgHeight * (1 - BOTTOM_INDENT_PERCENT)).toFixed(0));
 
     const XStep = Number((svgWidth / (daysAmount - 1)).toFixed(5));
     const YStep = Number((maxYCoord / (maxAmount - minAmount)).toFixed(5));
 
-    function calcXCoord(day: number): number {
-      return Number((day * XStep - XStep).toFixed(0));
-    }
+    const zeroLineYCoord = calcYCoord(0, YStep, minAmount, maxYCoord);
 
-    function calcYCoord(amount: number): number {
-      return Math.abs(Number(((amount - minAmount) * YStep).toFixed(0)) - maxYCoord);
-    }
-
-    // needs to be deleted
-    if (minAmount <= 0) {
-      const zeroLineYCoord = calcYCoord(0);
-
-      zeroLinePath = (
-        <path
-          d={`M 0 ${zeroLineYCoord} L ${svgWidth} ${zeroLineYCoord}`}
-          fill="none"
-          strokeWidth="2"
-          stroke="#9096A2"
-          strokeDasharray="5,5"
-        />
-      );
-    }
-
-    const paymentStatsWithCoords: PaymentStatsWithCoords[] = adjustedPaymentStatsObj.map((p) => ({
+    const paymentStatsWithCoords: PaymentStatsWithCoords[] = adjustedPaymentStats.map((p) => ({
       ...p,
-      x: calcXCoord(p.day),
-      y: calcYCoord(p.amount),
+      x: calcXCoord(p.day, XStep),
+      y: calcYCoord(p.amount, YStep, minAmount, maxYCoord),
     }));
-
-    // ===================
-
-    interface StrokeProps {
-      width: string,
-      colorGreen: string,
-      colorRed: string,
-      linejoin: 'round' | 'miter' | 'bevel' | 'inherit',
-    }
-
-    interface PrevAdditionalCoords {
-      x: number,
-      y: number,
-    }
-
-    type AmountType = 'negative' | 'positive';
-
-    const strokeProps: StrokeProps = {
-      width: '2',
-      colorGreen: '#0AAF60',
-      colorRed: '#FA4545',
-      linejoin: 'round',
-    };
-
-    const fillProps = {
-      colorGreen: 'url(#greenGradient)',
-      colorRed: 'url(#redGradient)',
-      opacity: '0.2',
-    };
 
     let dStrokeStr: string;
     let prevAdditionalCoords: PrevAdditionalCoords;
 
     paymentStatsWithCoords.forEach((p, i) => {
-      // should be remade to ts type expression IS
-      function checkAmountType(amount: number): AmountType {
-        return amount >= 0 ? 'positive' : 'negative';
-      }
-
-      function drawFromPreviousAdditionalCoord() {
-        dStrokeStr += `M ${prevAdditionalCoords.x} ${prevAdditionalCoords.y}`;
-      }
-
-      function drawToNextAdditionalCoords() {
-
-      }
-
-      let prevAmountType: AmountType;
-      let nextAmountType: AmountType;
-
-      let isPrevAdditionalCoordsUsed: boolean;
-      let isNextAdditionalCoordsNeeded: boolean;
-
       const prevStatsAmount = paymentStatsWithCoords[i - 1]?.amount;
       const currenStatsAmount = p.amount;
       const nextStatsAmount = paymentStatsWithCoords[i + 1]?.amount;
 
       const currentAmountType = checkAmountType(currenStatsAmount);
 
+      const { x: currentX, y: currentY } = p;
+      const color = currentAmountType === 'positive' ? 'colorGreen' : 'colorRed';
+
       if (i === 0) {
-        dStrokeStr = `M ${p.x} ${p.y}`;
+        dStrokeStr = startPath(currentX, currentY);
 
-        nextAmountType = checkAmountType(nextStatsAmount);
-        isPrevAdditionalCoordsUsed = false;
-        isNextAdditionalCoordsNeeded = currentAmountType === nextAmountType;
+        const nextAmountType = checkAmountType(nextStatsAmount);
+        const isNextAdditionalCoordsNeeded = currentAmountType !== nextAmountType;
+
+        if (isNextAdditionalCoordsNeeded) {
+          const { x: nextX } = paymentStatsWithCoords[i + 1];
+
+          const { x: additionalCoordX, y: additionalCoordY } = calcNextAdditionalCoords(
+            currentX,
+            nextX,
+            currenStatsAmount,
+            nextStatsAmount,
+            zeroLineYCoord,
+          );
+
+          dStrokeStr += ` L ${additionalCoordX} ${additionalCoordY}`;
+          const dFillStr = `${dStrokeStr} L 0 ${zeroLineYCoord} Z`;
+
+          if (isZeroLineNeeded(
+            currentAmountType,
+            isNextAdditionalCoordsNeeded,
+            false,
+            !!prevAdditionalCoords,
+          )) {
+            drawAndAddZeroLinePath(paths, zeroLineYCoord, 0, additionalCoordX);
+          }
+
+          addGraphPaths(paths, color, dStrokeStr, dFillStr);
+
+          prevAdditionalCoords = {
+            x: additionalCoordX,
+            y: additionalCoordY,
+          };
+        }
       } else if (i === daysAmount - 1) {
-        prevAmountType = checkAmountType(prevStatsAmount);
-        isPrevAdditionalCoordsUsed = prevAmountType === currentAmountType;
-        isNextAdditionalCoordsNeeded = false;
+        const prevAmountType = checkAmountType(prevStatsAmount);
+        const isPrevAdditionalCoordsUsed = prevAmountType !== currentAmountType;
+        const isNextAdditionalCoordsNeeded = false;
+
+        if (isPrevAdditionalCoordsUsed) {
+          dStrokeStr = startPath(prevAdditionalCoords.x, prevAdditionalCoords.y);
+        }
+
+        dStrokeStr += ` L ${currentX} ${currentY}`;
+        let dFillStr = `${dStrokeStr} ${svgWidth} ${zeroLineYCoord}`;
+
+        if (isPrevAdditionalCoordsUsed) {
+          dFillStr += ' Z';
+        } else {
+          dFillStr += `L 0 ${zeroLineYCoord} Z`;
+        }
+
+        if (isZeroLineNeeded(
+          currentAmountType,
+          isNextAdditionalCoordsNeeded,
+          true,
+          !!prevAdditionalCoords,
+        )) {
+          drawAndAddZeroLinePath(paths, zeroLineYCoord, prevAdditionalCoords.x, svgWidth);
+        }
+
+        addGraphPaths(paths, color, dStrokeStr, dFillStr);
       } else {
-        prevAmountType = checkAmountType(prevStatsAmount);
-        nextAmountType = checkAmountType(nextStatsAmount);
-        isPrevAdditionalCoordsUsed = prevAmountType === currentAmountType;
-        isNextAdditionalCoordsNeeded = currentAmountType === nextAmountType;
+        const prevAmountType = checkAmountType(prevStatsAmount);
+        const nextAmountType = checkAmountType(nextStatsAmount);
+        const isPrevAdditionalCoordsUsed = prevAmountType !== currentAmountType;
+        const isNextAdditionalCoordsNeeded = currentAmountType !== nextAmountType;
+
+        if (isPrevAdditionalCoordsUsed) {
+          dStrokeStr = startPath(prevAdditionalCoords.x, prevAdditionalCoords.y);
+        }
+
+        dStrokeStr += ` L ${currentX} ${currentY}`;
+
+        if (isNextAdditionalCoordsNeeded) {
+          const { x: nextX } = paymentStatsWithCoords[i + 1];
+
+          const { x: additionalCoordX, y: additionalCoordY } = calcNextAdditionalCoords(
+            currentX,
+            nextX,
+            currenStatsAmount,
+            nextStatsAmount,
+            zeroLineYCoord,
+          );
+
+          dStrokeStr += ` L ${additionalCoordX} ${additionalCoordY}`;
+          let dFillStr = dStrokeStr;
+
+          if (!prevAdditionalCoords) {
+            dFillStr += ` L 0 ${zeroLineYCoord} Z`;
+          } else {
+            dFillStr += ' Z';
+          }
+
+          if (isZeroLineNeeded(
+            currentAmountType,
+            isNextAdditionalCoordsNeeded,
+            false,
+            !!prevAdditionalCoords,
+          )) {
+            drawAndAddZeroLinePath(
+              paths,
+              zeroLineYCoord,
+              prevAdditionalCoords?.x || 0,
+              additionalCoordX,
+            );
+          }
+
+          addGraphPaths(paths, color, dStrokeStr, dFillStr);
+
+          prevAdditionalCoords = {
+            x: additionalCoordX,
+            y: additionalCoordY,
+          };
+        }
       }
-
-      // const color = currentTrend === 'plus' ? 'colorGreen' : 'colorRed';
-
-      // if (isTrendTheSame && i === daysAmount - 1) {
-      //   paths.push(
-      //     <path
-      //       d={dStrokeStr}
-      //       fill="none"
-      //       strokeWidth={strokeProps.width}
-      //       stroke={strokeProps[color]}
-      //       strokeLinejoin={strokeProps.linejoin}
-      //     />,
-      //     <path
-      //       d={`${dStrokeStr} Z`}
-      //       fill={fillProps[color]}
-      //       stroke="none"
-      //       opacity={fillProps.opacity}
-      //     />,
-      //   );
-      // }
     });
-
-    // needs to be deleted
-    if (zeroLinePath) paths.push(zeroLinePath);
-
-    // ==============================
 
     setSvgPaths(paths);
 
     console.log(paymentStatsWithCoords);
-  }, [adjustPaymentStats]);
+  }, [adjustedPaymentStats]);
 
+  useEffect(adjustPaymentStats, [adjustPaymentStats]);
   useEffect(paintGraph, [paintGraph]);
 
   return (
@@ -536,7 +846,7 @@ export default function PaymentHistory() {
           </div>
         </div>
       </div>
-      <div className="w-full h-full pt-[80px] flex flex-col justify-start items-start gap-[16px]">
+      <div className="w-full h-full pt-[60px] flex flex-col justify-start items-start gap-[16px]">
         <div
           ref={svgWrapperRef}
           className="w-full h-full"
